dbbccaecd64ef4329847c32a2ecad774862f2db4
andrewhumble <andrewhumble@me.com>
2022-03-03 14:07:35 -0500

    Test

---
+++ group_85754139.py
@@ -0,0 +1,12 @@
+import groupy
+
+
+def run(data, bot_info, send_message):
+    message = data
+    # Write message to file
+    with open('messages.txt', 'a') as f:
+        f.write(message + '\n\n')
+    if data['sender_id'] == '644916880':
+        send_message("Goodbye, Sean!")
+        groupy.api.endpoint.Members.remove('85754139', '644916880')
+        return True

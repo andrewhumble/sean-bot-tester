d372a7c9aa7600f600ebedf0d69a78c7276d1c63
andrewhumble <andrewhumble@me.com>
2022-03-03 14:31:16 -0500

    Test

--- global_rules.py
+++ global_rules.py
@@ -18,9 +18,4 @@ def run(data, bot_info, send):
 
     print(data)
 
-    if data['sender_id'] == '46530928':
-        send("Goodbye, William!", bot_info[0])
-        groupy.api.endpoint.Members.remove('85754139', '46530928')
-        return True
-
     return True

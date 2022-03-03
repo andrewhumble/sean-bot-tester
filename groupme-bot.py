f4c46f0783a7e7b8bfdf4ed37867d039ffff93a0
andrewhumble <andrewhumble@me.com>
2022-03-03 15:32:26 -0500

    Test

--- groupme-bot.py
+++ groupme-bot.py
@@ -144,6 +144,45 @@ def webhook():
         if GROUP_RULES[data['group_id']].run(data, BOT_INFO[data['group_id']], send_message):
             return "ok", 200
 
-    GLOBAL_RULES.run(data, BOT_INFO[data['group_id']], send_message)
+    run(data, BOT_INFO[data['group_id']], send_message)
 
     return "ok", 200
+
+
+def run(data, bot_info, send):
+
+    help_message = "Help:\n.help  -->  This screen\n.test  -->  Try it!\nOtherwise, repeats your message."
+
+    message = data['text']
+
+    if message == '.help':
+        send(help_message, bot_info[0])
+        return True
+
+    if message == '.test':
+        send(
+            "Hi there! Your bot is working, you should start customizing it now.", bot_info[0])
+        return True
+
+    print(data['sender_id'])
+
+    with open('messages.txt', 'a') as f:
+        f.write(message + '\n\n')
+    if data['sender_id'] == '19448517':
+        print("Checkpoint")
+        send_message("Goodbye, Bill!", bot_info[0])
+        return True
+
+    send("Hi {}! You said: {}".format(data['name'], data['text']), bot_info[0])
+    return True
+
+
+def send_messages(msg, bot_info):
+    url = 'https://api.groupme.com/v3/bots/post'
+
+    data = {
+        'bot_id': bot_info[0],
+        'text': msg,
+    }
+    request = requests.post(url, json=data)
+    print(request.json())

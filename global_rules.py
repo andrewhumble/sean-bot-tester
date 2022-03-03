7e3e8a12e2f98554ecf6a9ae4258e1fb9b7b8334
andrewhumble <andrewhumble@me.com>
2022-03-03 14:32:56 -0500

    Test

--- global_rules.py
+++ global_rules.py
@@ -1,6 +1,3 @@
-import groupy
-
-
 def run(data, bot_info, send):
 
     help_message = "Help:\n.help  -->  This screen\n.test  -->  Try it!\nOtherwise, repeats your message."
@@ -16,4 +13,5 @@ def run(data, bot_info, send):
             "Hi there! Your bot is working, you should start customizing it now.", bot_info[0])
         return True
 
+    send("Hi {}! You said: {}".format(data['name'], data['text']), bot_info[0])
     return True
